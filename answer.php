33673e4bfb3ee247f477651b82eecbab05bc98d1
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2022-11-13 07:53:07 +0900

    R4.11.13 answer.php変更

--- answer.php
+++ answer.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/astyle.css">
+</head>
+<body>
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 
@@ -13,3 +23,5 @@
 
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
+</body>
+</html>
